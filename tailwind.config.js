2211641d18dde0703e530f6e8a1e1905dbfff3a7
raymondz <raykyri@gmail.com>
2021-11-19 00:32:54 -0500

    add home feed, mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      mono: ['Overpass Mono', 'monospace']
+    },
     extend: {
       colors: {
         pink: '#d562f2'
