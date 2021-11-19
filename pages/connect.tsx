ef9a22da7743e14a4804ff95c0fcac22781f44c1
raymondz <raykyri@gmail.com>
2021-11-19 13:41:31 -0500

    adjust input buttons

--- pages/connect.tsx
+++ pages/connect.tsx
@@ -1,18 +1,39 @@
-import React from "react"
+import React, { useRef } from "react"
 import { Header } from "components/Header"
 
 export default function ConnectPage(props: {}) {
-	const token = "4ecce3f8da39753fb31fac6c5f4061efb1337f0ef768f19a8b56df4cfb2aafeb"
+	const twitterRef = useRef()
+	const token =
+		"4ecce3f8da39753fb31fac6c5f4061efb1337f0ef768f19a8b56df4cfb2aafeb"
 	return (
 		<div className="max-w-lg m-auto font-mono">
 			<Header />
 			<div className="border border-gray-300 rounded-xl p-6">
-				<div>Sign your secret token with your Ethereum address. This will link it to your public identity:</div>
+				<div>
+					Verify ownership of your secret token by posting a signed message to
+					Twitter:
+				</div>
+				<input
+					className="block w-full rounded-xl px-4 py-2 mt-6"
+					type="text"
+					placeholder="@SatoshiNakamoto"
+					ref={twitterRef}
+				/>
 				<input
 					className="block w-full cursor-pointer bg-pink hover:bg-midpink text-white rounded-xl px-4 py-2 mt-6"
 					type="button"
-					value="Sign with Ethereum wallet"
-					onClick={() => { }}
+					value="Sign and post to Twitter"
+					onClick={() => {
+						console.log(twitterRef.current.value)
+					}}
+				/>
+				<input
+					className="block w-full cursor-pointer bg-gray-300 text-gray-800 rounded-xl px-4 py-2 mt-6"
+					type="button"
+					value="Back"
+					onClick={() => {
+						window.history.go(-1)
+					}}
 				/>
 			</div>
 		</div>
